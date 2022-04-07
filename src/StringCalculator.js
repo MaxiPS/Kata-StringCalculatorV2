6ef30b3dc6288945d3f2be4ea13fa12652f25f36
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-04-07 10:40:09 +0100

    refactor and new implementations

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,26 +1,39 @@
 const sum = (line) => {
   const numbers = filterOnlyNumbers(line);
+  let result = 0;
+  let negativeNumbers = [];
 
   if (numbers.length !== 0) {
-    let result = 0;
     numbers.forEach((number) => {
-      if (isNaN(parseInt(number))) number = 0;
+      if (number < 0) {
+        negativeNumbers.push(number);
+      }
+      if (isNaN(parseInt(number)) || parseInt(number) >= 1000) {
+        number = 0;
+      }
       result += parseInt(number);
     });
+
+    if (negativeNumbers.length !== 0) {
+      throw new Error(`Negatives not allowed: ${negativeNumbers.join(", ")}`);
+    }
+
     return result;
   }
-  return "Unexpected error"
 };
-
-
 
 const filterOnlyNumbers = (line) => {
   if (line.includes("\n")) {
+    console.log(line.split("\n".trim()));
     const splitByJumpLine = line.split("\n".trim());
     return splitByJumpLine.filter((number) => !isNaN(parseInt(number)));
   }
   return line.split(",");
 };
+
+// //^(.*?)//\n
+// //[separator]\n1;2
+// const [separator, ...list] = line.split("\n")
 
 module.exports = {
   sum,
--- src/test/StringCalculator.test.js
+++ src/test/StringCalculator.test.js
@@ -12,29 +12,51 @@
 const { sum } = require("../StringCalculator");
 
 describe("StringCalculator Should", () => {
-  test("return_the_same_number_given_a_single_number", () => {
+  test("return the same number given a single number", () => {
     expect(sum("1")).toBe(1);
+    expect(sum("2")).toBe(2);
   });
 
-  test("return_zero_given_an_empty_line", () => {
+  test("return zero given an empty line", () => {
     expect(sum("")).toBe(0);
     expect(sum("  ")).toBe(0);
   });
 
-  test("return_the_result_of_a_sum", () => {
+  test("return the result of a sum when the delimeter is a coma", () => {
     expect(sum("1,2")).toBe(3);
+    expect(sum("12,2")).toBe(14);
   });
 
-  test("ignore_all_not_integer_numbers", () => {
+  test("ignore all not integer numbers", () => {
     expect(sum("1,2,,")).toBe(3);
     expect(sum(",,1,2,")).toBe(3);
-    expect(sum("1,2,A,,")).toBe(3);
+    expect(sum("11,2.1,A,,")).toBe(13);
     expect(sum("1, ,A,2,")).toBe(3);
     expect(sum("1,   ,A,2,,,")).toBe(3);
   });
 
-  test("return_the_result_when_the_line_contains_jumps_line_between_numbers", () => {
+  test("return the result even when the line contains jumps line between numbers", () => {
     expect(sum("1\n2,3")).toBe(6);
     expect(sum("1,\n")).toBe(1);
+    expect(sum("\n\n,1,\n2")).toBe(3);
+  });
+
+  test("sum the numbers even when they are separated by custom delimiters", () => {
+    expect(sum("//;\n1;2;3")).toBe(6);
+    expect(sum("//[*][%]\n1*2%3")).toBe(6);
+    expect(sum("“//[***]\n1***2***3”")).toBe(6);
+  });
+
+  test("throw an exception if the line contains negative numbers", () => {
+    expect(() => sum("1,-2,-3")).toThrowError("Negatives not allowed: -2, -3");
+    expect(() => sum("1,-2,-3,1,-5")).toThrowError(
+      "Negatives not allowed: -2, -3, -5"
+    );
+  });
+
+  test("ignore numbers greater and equal to 1000", () => {
+    expect(sum("1000,2")).toBe(2);
+    //TODO: Arreglar esto!
+   expect(sum("\n1000,2")).toBe(2);
   });
 });
